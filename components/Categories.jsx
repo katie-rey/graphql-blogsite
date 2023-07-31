ef1359f65989b2494c3bc5bbc5feaa200224f37e
Katie Rey <96268293+katie-rey@users.noreply.github.com>
2023-07-31 15:03:28 +1200

    Fixx errors

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -12,11 +12,15 @@ const Categories = ({ catergories, slug }) => {
 
   // console.log(categories)
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 mb-8 pb-12">
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">Categories</h3>
-      {categories.map((category) => (
-        <Link key={category.id} href={`/category/${category.slug}`}>
-          <span className="cursor-pointer font-semibold block pb-3 mb-3">
+      {categories.map((category, index) => (
+        <Link key={index} href={`/category/${category.slug}`}>
+          <span
+            className={`cursor-pointer block ${
+              index === categories.length - 1 ? 'border-b-0' : 'border-b'
+            } pb-3 mb-3`}
+          >
             {category.name}
           </span>
         </Link>
--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -30,13 +30,13 @@ const PostCard = ({ post }) => (
     </h1>
     <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
       <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center">
-        <img
-          unoptimized
+        <Image
+          unoptimized="true"
           loader={graphCMSImageLoader}
           alt={post.author.name}
-          height="30px"
-          width="30px"
-          className="align-middle rounded-full"
+          height={30}
+          width={30}
+          className="align-middle ed-full"
           src={post.author.photo.url}
         />
         <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">
